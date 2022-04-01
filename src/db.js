64bff2fff2f1524605cf95b6d0ac9fc8523bdfd2
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-01 09:53:14 +0200

    added stylesheet

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import "firebase/database"
+
+const config = {
+    //APIKeys
+    apiKey: "AIzaSyAG7rYv4Jstyh4oMJZM89fLfZR5c1bxYPE",
+    authDomain: "firechat-254ee.firebaseapp.com",
+    projectId: "firechat-254ee",
+    storageBucket: "firechat-254ee.appspot.com",
+    messagingSenderId: "645038757756",
+    appId: "1:645038757756:web:b5124ebf7bc683338876f9"
+}
+
+const db = firebase.initializeApp(config)
+
+export default db
